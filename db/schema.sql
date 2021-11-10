afd1b9ad32dab36bc0b95c30e22bfb48b30d773b
Carson Johnson <carson74johnson@gmail.com>
2021-11-09 19:26:53 -0500

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -14,6 +14,7 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
@@ -21,12 +22,14 @@ CREATE TABLE role (
 
 DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
-    id INT NO NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
+    role_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE SET NULL,
+    manager_id INT,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
 )
