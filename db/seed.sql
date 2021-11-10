285f0e280d152262a8f01c6968e96463d8963b37
Carson Johnson <carson74johnson@gmail.com>
2021-11-09 19:31:36 -0500

    fixed typos in seed.sql file

--- db/seed.sql
+++ db/seed.sql
@@ -1,10 +1,10 @@
-INSERT INTO departments (name)
+INSERT INTO department (name)
 VALUES ("Sales"),
        ("Engineering"),
        ("Finance"),
        ("Legal");
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Lead", 100000, 1),
        ("Salesperson", 80000, 1),
        ("Lead Engineer", 150000, 2),
@@ -14,7 +14,7 @@ VALUES ("Sales Lead", 100000, 1),
        ("Legal Team Lead", 250000, 4),
        ("Lawyer", 190000, 4);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Doe", 1, null),
 	   ("Mike", "Chan", 2, 1),
        ("Ashley", "Rodriguez", 3, null),
